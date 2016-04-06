270ec25e39477a58e2a38bb2c700abcc793f2ef8
skyline75489 <skyline75489@outlook.com>
2016-04-06 10:08:46 +0800

    framework project

---
+++ JLAlertView/JLAlertView.h
@@ -0,0 +1,19 @@
+//
+//  JLAlertView.h
+//  JLAlertView
+//
+//  Created by skyline on 16/4/6.
+//  Copyright © 2016年 skyline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JLAlertView.
+FOUNDATION_EXPORT double JLAlertViewVersionNumber;
+
+//! Project version string for JLAlertView.
+FOUNDATION_EXPORT const unsigned char JLAlertViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JLAlertView/PublicHeader.h>
+
+
