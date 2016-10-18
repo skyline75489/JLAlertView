ee64fe1a5ebf06b2793b32dab41b7988cfbd811a
skyline75489 <skyline75489@outlook.com>
2016-10-18 22:41:02 +0800

    Demo project xcode 8 & swift 3

--- JLAlertViewDemo/JLAlertViewDemo/ViewController.swift
+++ JLAlertViewDemo/JLAlertViewDemo/ViewController.swift
@@ -16,14 +16,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        alertButton.addTarget(self, action: #selector(showSimpleAlert), forControlEvents: .TouchUpInside)
-        textfieldAlertButton.addTarget(self, action: #selector(showTextFieldAlert), forControlEvents: .TouchUpInside)
-        imageAlertButton.addTarget(self, action: #selector(showImageAlert), forControlEvents: .TouchUpInside)
+        alertButton.addTarget(self, action: #selector(showSimpleAlert), for: .touchUpInside)
+        textfieldAlertButton.addTarget(self, action: #selector(showTextFieldAlert), for: .touchUpInside)
+        imageAlertButton.addTarget(self, action: #selector(showImageAlert), for: .touchUpInside)
     }
 
     func showSimpleAlert() {
         JLAlertView(title: "Default Style", message: "Standard Alert")
-        .addButttonWithTitle("Cancel", style: .Cancel, action: nil)
+        .addButttonWithTitle("Cancel", style: .cancel, action: nil)
         .addButttonWithTitle("OK", action:nil)
         .show()
     }
@@ -36,7 +36,7 @@ class ViewController: UIViewController {
         .addTextFieldWithConfigurationHandler({ (textField) in
             textField.placeholder = "Password"
         })
-        .addButttonWithTitle("Cancel", style: .Cancel, action: nil)
+        .addButttonWithTitle("Cancel", style: .cancel, action: nil)
         .addButttonWithTitle("OK", action:{(title, alert) in
             let username = alert.textFields[0].text
             let password = alert.textFields[1].text
@@ -49,7 +49,7 @@ class ViewController: UIViewController {
     func showImageAlert() {
         JLAlertView(title: "Default Style", message: "Standard Alert")
         .addImage(UIImage(named: "ios-announce")!)
-        .addButttonWithTitle("Cancel", style: .Cancel, action: nil)
+        .addButttonWithTitle("Cancel", style: .cancel, action: nil)
         .addButttonWithTitle("OK", action:nil)
         .show()
     }
